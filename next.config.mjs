fa1a94435f1942c3b69135abed5c7db74c1ea448
Malena205 <malena-schmidt2000@web.de>
2024-09-08 16:01:01 +0100

    Changes to host website on github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  trailingSlash: true,
 };
 
 export default nextConfig;
