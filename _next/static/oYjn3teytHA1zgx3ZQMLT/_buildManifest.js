95904fab8aecc32f5fd4b0249f079bf8f052b86b
Malena205 <Malena205@users.noreply.github.com>
2025-11-16 12:19:05 +0000

    deploy: 72c471cf6a39c53b20c0846c677f192a460d246b

---
+++ _next/static/oYjn3teytHA1zgx3ZQMLT/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-bd2b38500e7a20cf.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
